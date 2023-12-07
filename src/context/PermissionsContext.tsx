1525bfde856e68bce4c6ee21e5abd290832288a7
Jeral Pineda <jeral.cris@gmail.com>
2023-12-06 22:43:33 -0600

    fix: revisar el permiso del GPS al regresar a la app

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,6 +1,11 @@
-import React, {createContext, useState} from "react";
-import {Platform} from "react-native";
-import {PERMISSIONS, PermissionStatus, request} from "react-native-permissions";
+import React, {createContext, useEffect, useState} from "react";
+import {AppState, Platform} from "react-native";
+import {
+  PERMISSIONS,
+  PermissionStatus,
+  check,
+  request,
+} from "react-native-permissions";
 
 export interface PermissionsState {
   locationStatus: PermissionStatus;
@@ -23,19 +28,28 @@ export const PermissionsContext = createContext({} as PermissionsContextProps);
 export const PermissionsProvider = ({children}: any) => {
   const [permissions, setPermissions] = useState(permissionInitState);
 
+  useEffect(() => {
+    const listener = AppState.addEventListener("change", state => {
+      if (state !== "active") {
+        return;
+      }
+      checkLocationPermission();
+    });
+
+    return () => {
+      listener.remove();
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const askLocationPermission = async () => {
     let permissionStatus: PermissionStatus;
 
     if (Platform.OS === "ios") {
-      //* Saber si tiene acceso a la localización en IOS
-      // permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-
       //* Solicitar permiso a la localización cuando se usa IOS, si ya tiene el acceso no lo pide nuevamente
       permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
     } else {
-      //* Saber si tiene acceso a la localización en IOS
-      // permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-
       //* Solicitar permiso a la localización cuando se usa IOS
       permissionStatus = await request(
         PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
@@ -48,7 +62,22 @@ export const PermissionsProvider = ({children}: any) => {
     });
   };
 
-  const checkLocationPermission = () => {};
+  const checkLocationPermission = async () => {
+    let permissionStatus: PermissionStatus;
+
+    if (Platform.OS === "ios") {
+      //* Saber si tiene acceso a la localización en IOS
+      permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else {
+      //* Saber si tiene acceso a la localización en IOS
+      permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    }
+
+    setPermissions({
+      ...permissions,
+      locationStatus: permissionStatus,
+    });
+  };
 
   return (
     <PermissionsContext.Provider
