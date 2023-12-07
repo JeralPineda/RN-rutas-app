100bd5911606c92ab27506e31198a4cd96392fb7
Jeral Pineda <jeral.cris@gmail.com>
2023-12-06 22:22:58 -0600

    fix: haciendo uso del PermissionContext

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,5 +1,6 @@
 import React, {createContext, useState} from "react";
-import {PermissionStatus} from "react-native-permissions";
+import {Platform} from "react-native";
+import {PERMISSIONS, PermissionStatus, request} from "react-native-permissions";
 
 export interface PermissionsState {
   locationStatus: PermissionStatus;
@@ -22,7 +23,31 @@ export const PermissionsContext = createContext({} as PermissionsContextProps);
 export const PermissionsProvider = ({children}: any) => {
   const [permissions, setPermissions] = useState(permissionInitState);
 
-  const askLocationPermission = () => {};
+  const askLocationPermission = async () => {
+    let permissionStatus: PermissionStatus;
+
+    if (Platform.OS === "ios") {
+      //* Saber si tiene acceso a la localización en IOS
+      // permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+
+      //* Solicitar permiso a la localización cuando se usa IOS, si ya tiene el acceso no lo pide nuevamente
+      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    } else {
+      //* Saber si tiene acceso a la localización en IOS
+      // permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+
+      //* Solicitar permiso a la localización cuando se usa IOS
+      permissionStatus = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
+    }
+
+    setPermissions({
+      ...permissions,
+      locationStatus: permissionStatus,
+    });
+  };
+
   const checkLocationPermission = () => {};
 
   return (
--- src/screens/PermissionsScreen.tsx
+++ src/screens/PermissionsScreen.tsx
@@ -1,40 +1,18 @@
-import React from "react";
-import {Button, Platform, StyleSheet, Text, View} from "react-native";
-import {
-  PERMISSIONS,
-  PermissionStatus,
-  // check,
-  request,
-} from "react-native-permissions";
+import React, {useContext} from "react";
+import {Button, StyleSheet, Text, View} from "react-native";
+
+import {PermissionsContext} from "../context/PermissionsContext";
 
 export const PermissionsScreen = () => {
-  const checkLocationPermission = async () => {
-    let permissionStatus: PermissionStatus;
-
-    if (Platform.OS === "ios") {
-      //* Saber si tiene acceso a la localización en IOS
-      // permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-
-      //* Solicitar permiso a la localización cuando se usa IOS, si ya tiene el acceso no lo pide nuevamente
-      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-    } else {
-      //* Saber si tiene acceso a la localización en IOS
-      // permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-
-      //* Solicitar permiso a la localización cuando se usa IOS
-      permissionStatus = await request(
-        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-      );
-    }
-
-    console.log(permissionStatus);
-  };
+  const {permissions, askLocationPermission} = useContext(PermissionsContext);
 
   return (
     <View style={styles.container}>
       <Text>PermissionsScreen</Text>
 
-      <Button title="Permiso" onPress={checkLocationPermission} />
+      <Button title="Permiso" onPress={askLocationPermission} />
+
+      <Text>{JSON.stringify(permissions, null, 5)}</Text>
     </View>
   );
 };
